ec782074a40437ea8c3a2dd5b282edd696a4980e
EmilS <emil.s21165@gmail.com>
2022-10-11 17:02:09 +0600

    added React.lazy and Suspense functions

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,10 +1,12 @@
 import {BrowserRouter as Router, Route, Routes} from "react-router-dom";
-
+import { lazy, Suspense } from "react";
 import AppHeader from "../appHeader/AppHeader";
-import Page404 from "../pages/404";
-import ComicsPage from "../pages/ComicsPage";
-import MainPage from "../pages/MainPage";
-import SingleComicPage from "../pages/SingleComicPage";
+import Spinner from "../spinner/Spinner";
+
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
 
 
 const App = () => {
@@ -13,12 +15,14 @@ const App = () => {
          <div className="app">
             <AppHeader/>
             <main>
+                <Suspense fallback={<Spinner/>}>
                 <Routes>
                     <Route path="/" element={<MainPage/>}/>
                     <Route path="/comics" element={<ComicsPage/>}/>
                     <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
                     <Route path="*" element={<Page404/>}/>
                 </Routes>
+                </Suspense>
             </main>
         </div>
        </Router>
