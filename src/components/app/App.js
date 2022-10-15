f896859b78a2a33fd4436921f30fec555958fb6b
EmilS <emil.s21165@gmail.com>
2022-10-15 21:46:06 +0600

    added Character search form using 'Formik' and 'Yup'

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -6,7 +6,9 @@ import Spinner from "../spinner/Spinner";
 const Page404 = lazy(() => import('../pages/404'));
 const MainPage = lazy(() => import('../pages/MainPage'));
 const ComicsPage = lazy(() => import('../pages/ComicsPage'));
-const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+const SingleComicLayout = lazy(() => import('../pages/singleComicLayout/SingleComicLayout'));
+const SingleCharacterLayout = lazy(() => import('../pages/singleCharacterLayout/SingleCharacterLayout'));
+const SinglePage = lazy(() => import('../pages/SinglePage'));
 
 
 const App = () => {
@@ -19,7 +21,13 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<MainPage/>}/>
                     <Route path="/comics" element={<ComicsPage/>}/>
-                    <Route path="/comics/:comicId" element={<SingleComicPage/>}/>
+                    <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout}/>}/>
+                        {/* <SinglePage Component={SingleComicLayout} dataType='comic'/>
+                    </Route> */}
+                    <Route path="/comics/:id" element={<SinglePage Component={SingleCharacterLayout}/>}/>
+                    {/* <Route exact path="/characters/:id">
+                        <SinglePage Component={SingleCharacterLayout} dataType='character'/>
+                    </Route> */}
                     <Route path="*" element={<Page404/>}/>
                 </Routes>
                 </Suspense>
