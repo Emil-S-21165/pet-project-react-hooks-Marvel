675f630ff05ff4789faaecb3b5efca91e70d97c5
EmilS <emil.s21165@gmail.com>
2022-10-17 11:53:52 +0600

    bags fixed

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,7 +10,6 @@ const SingleComicLayout = lazy(() => import('../pages/singleComicLayout/SingleCo
 const SingleCharacterLayout = lazy(() => import('../pages/singleCharacterLayout/SingleCharacterLayout'));
 const SinglePage = lazy(() => import('../pages/SinglePage'));
 
-
 const App = () => {
     return (
        <Router>
@@ -21,13 +20,8 @@ const App = () => {
                 <Routes>
                     <Route path="/" element={<MainPage/>}/>
                     <Route path="/comics" element={<ComicsPage/>}/>
-                    <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout}/>}/>
-                        {/* <SinglePage Component={SingleComicLayout} dataType='comic'/>
-                    </Route> */}
-                    <Route path="/comics/:id" element={<SinglePage Component={SingleCharacterLayout}/>}/>
-                    {/* <Route exact path="/characters/:id">
-                        <SinglePage Component={SingleCharacterLayout} dataType='character'/>
-                    </Route> */}
+                    <Route path="/comics/:id" element={<SinglePage Component={SingleComicLayout} dataType='comic' />} />
+                    <Route path="/characters/:id" element={<SinglePage Component={SingleCharacterLayout} dataType='character'/>} />
                     <Route path="*" element={<Page404/>}/>
                 </Routes>
                 </Suspense>
--- src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
+++ src/components/pages/singleCharacterLayout/SingleCharacterLayout.js
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import './singleCharacterLayout.scss';
 
 const SingleCharacterLayout = ({data}) => {
@@ -5,12 +6,13 @@ const SingleCharacterLayout = ({data}) => {
     const {name, description, thumbnail} = data;
 
     return (
-        <div className="single-comic">
-            <img src={thumbnail} alt={name} className="single-comic__char-img"/>
-            <div className="single-comic__info">
-                <h2 className="single-comic__name">{name}</h2>
-                <p className="single-comic__descr">{description}</p>
+        <div className="single-char">
+            <img src={thumbnail} alt={name} className="single-char__char-img"/>
+            <div className="single-char__info">
+                <h2 className="single-char__name">{name}</h2>
+                <p className="single-char__descr">{description}</p>
             </div>
+            <Link to="/" className="single-char__back">Back to main</Link>
         </div>
     )
 }
